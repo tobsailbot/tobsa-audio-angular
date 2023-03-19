35879ba4738642dde67893532dca5cf1219ba52c
tobsailbot <tobsailbot@gmail.com>
2023-03-19 16:47:30 -0300

    products container

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -12,7 +12,7 @@ export class ProductsComponent implements OnInit {
 
   products:any = products;
 
-  // products:any = [];
+   //products:any = [];
   ngOnInit(): void {
 
     // this.storeApiService.getProducts().subscribe(data => {
